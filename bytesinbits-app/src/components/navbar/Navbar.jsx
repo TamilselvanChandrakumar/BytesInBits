2aec1e9a000de6c913beabe5d6c0e95061a9ea47
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-02-11 19:17:52 +0530

    first commit

--- bytesinbits-app/src/components/navbar/Navbar.jsx
+++ bytesinbits-app/src/components/navbar/Navbar.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MdDensityMedium } from "react-icons/md";
 import "../navbar/Navbar.css";
 import companylogo from "../../assets/logo/image 1.png";
 const Navbar = () => {
   const [sideBar, setSideBar] = useState(false);
+  const [sticky, setSticky] = useState(false);
   // sidebar
   const handleSideBar = () => {
     setSideBar(!sideBar);
   };
+  // hendle scrollbar
+  useEffect(() => {
+    const handleScrollbar = () => {
+      setSticky(window.scrollY > 20);
+    };
+    window.addEventListener("scroll", handleScrollbar);
+    return () => {
+      window.removeEventListener("scroll", handleScrollbar);
+    };
+  });
   return (
     <>
-      <div className="navbar_container">
+      <div className={`navbar_container ${sticky ? "sticky" : null}`}>
         <div className="navbar_inner">
           <nav className="navbar">
             <div className="navbar_brand">
