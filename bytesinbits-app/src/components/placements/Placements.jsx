73692ea19f6abd0088e96e11cf80689018c890c4
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-02-15 13:41:54 +0530

    first commit

--- bytesinbits-app/src/components/placements/Placements.jsx
+++ bytesinbits-app/src/components/placements/Placements.jsx
@@ -1,58 +1,31 @@
 import React from "react";
 import "../../components/placements/Placements.css";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import placement1 from "../../assets/placements/placement1.png";
 import placement2 from "../../assets/placements/placement2.png";
 import placement3 from "../../assets/placements/placement3.png";
+import { MdArrowBackIos } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
 const Placements = () => {
-  let settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    swipeToSlide: true,
-    afterChange: function (index) {
-      console.log(
-        `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
-      );
-    },
-    responsive: [
-      {
-        breakpoint: 1300,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: true,
-        },
-      },
-    ],
+  let box = document.querySelector(".placement_card_container");
+  const prevbtnclick = () => {
+    let width = box.clientWidth;
+    box.scrollLeft = box.scrollLeft - width;
+  };
+  const nextbtnclick = () => {
+    let width = box.clientWidth;
+    box.scrollLeft = box.scrollLeft + width;
   };
   return (
     <>
-      <div className="placement_card_container slider-container">
-        <Slider {...settings}>
-          <div className="placement_card">
+      <div className="placement_card_carousel">
+        <button className="pre_btn" onClick={prevbtnclick}>
+          <MdArrowForwardIos />
+        </button>
+        <button className="next-btn" onClick={nextbtnclick}>
+          <MdArrowBackIos />
+        </button>
+        <div className="placement_card_container">
+          <div className="placement_card ">
             <img src={placement1} alt=""></img>
           </div>
           <div className="placement_card">
@@ -73,7 +46,7 @@ const Placements = () => {
           <div className="placement_card">
             <img src={placement2} alt=""></img>
           </div>
-        </Slider>
+        </div>
       </div>
     </>
   );
