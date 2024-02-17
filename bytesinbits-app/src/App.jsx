47dc2f332f063439ad9dc4027454da194e910224
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-02-18 01:14:21 +0530

    first commit

--- bytesinbits-app/src/App.jsx
+++ bytesinbits-app/src/App.jsx
@@ -11,10 +11,33 @@ import Aboutbytes from "./components/aboutus/aboutbytesinbits/Aboutbytes";
 import Blog from "./components/blog/Blog";
 import Placements from "./components/placements/Placements";
 import Contactus from "./components/contactus/Contactus";
+import { FaCircleArrowUp } from "react-icons/fa6";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [topToScroll, setTopToScroll] = useState(false);
+  const moveToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  };
+  useEffect(() => {
+    const handleScrollbar = () => {
+      setTopToScroll(window.scrollY > 20);
+    };
+    window.addEventListener("scroll", handleScrollbar);
+    return () => {
+      window.removeEventListener("scroll", handleScrollbar);
+    };
+  });
   return (
     <>
+      <div className={`${topToScroll ? "topscroll" : "removetopscroll"}`}>
+        <FaCircleArrowUp
+          size={50}
+          color=" #ffb400"
+          onClick={moveToTop}
+        ></FaCircleArrowUp>
+      </div>
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
